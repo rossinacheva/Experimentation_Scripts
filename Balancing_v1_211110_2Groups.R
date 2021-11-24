756db880ba1da204af2b8947b71381463f89a0c5
rossinacheva <83715067+rossinacheva@users.noreply.github.com>
2021-11-24 11:47:36 +0200

    Add files via upload

---
+++ Balancing_v1_211110_2Groups.R
@@ -0,0 +1,181 @@
+
+lapply(list('plyr','dplyr','boot','lubridate','samplesize'), library, character.only = TRUE)
+
+starttime = Sys.time()
+
+##########################################
+### Parameters
+##########################################
+
+
+input<- read.csv("/Users/r.nacheva/Downloads/Radius_Test_Nov_2021_11_24.csv")
+
+summary(input)
+
+names(input)
+
+input= subset(input, input$Supply_hours > 0)
+
+head(input)
+# Define segmentation : city, service, total
+input$segment_start = substr(input$starting_segment, 1, 6)
+
+
+# Define segmentation : COUNTRY, SEASON, Total
+segmentation <-  list(segment1 = c('segment_start','period')
+)
+# Define your entity
+entity <- 'id_driver'
+
+# Define new KPIs
+input$accR = input$acc/input$broadcast
+input$util = input$Active_hours / input$Supply_hours
+
+# Main KPIs you want to balance on and take into concideration for outlier detection
+balancing <- c('Supply_hours','active_days_period','driver_earning','Rides','Revenue','acc','accR','util')
+
+# Buckets split for treatment - put 0.5 for 50% treatment (and 50% control)
+buckets = 0.5
+
+# Parameter for allowed mean difference & outlier detection
+mean_difference = 0.05
+outlier_threshold = 0.999
+
+# Define Output file name
+outputname <- '/Users/r.nacheva/Downloads/Balancing_CL_Radius_Nov_211124.csv'
+
+
+############## End manual input
+
+# Generate random numbers as preparation for balancing
+input$ID <- seq.int(nrow(input))
+
+# Create empty datasets
+All <-c()
+Outliers_Total <-c()
+
+# Turn back to numeric values
+col_factor <- sapply(input, is.factor)
+input[col_factor] <- lapply(input[col_factor], as.character)
+num_cols <- sapply(input, is.numeric)
+input[,num_cols][is.na(input[,num_cols])] <- 0
+
+
+input[,col_factor][is.na(input[,col_factor])] <- 0
+
+
+
+# Sample size calculation --> not included for now 
+
+
+# Remove outliers from total data set 
+for (i in 1: length(balancing)) {
+  Outlier_1KPI <-subset(input, input[[balancing[i]]] > quantile(input[[balancing[i]]], outlier_threshold, na.rm = TRUE))
+  
+  Outliers_Total <-rbind(Outliers_Total,Outlier_1KPI)
+}
+
+input<-anti_join(input, Outliers_Total, by = entity)  
+
+# loop through segmentation levels
+for (level in 1:length(segmentation)){
+  
+  if (tolower(segmentation[level][[1]][1]) == "total"){
+    segment_input <- input
+    combinations = 1
+  }
+  else {
+    unique_combinations <- unique(input[,segmentation[level][[1]], drop = FALSE])
+    # filter out missing values
+    unique_combinations <- na.omit(unique_combinations)
+    combinations = nrow(unique_combinations)
+  }
+  
+  for(i_comb in 1:combinations){
+    
+    if (tolower(segmentation[level][[1]][1]) == "total"){
+      segment_input <- input
+    }
+    else {
+      segment_input <- merge(unique_combinations[i_comb,,drop = FALSE], input)
+      
+    }
+    segmentName = ifelse(tolower(segmentation[level][[1]][1]) == "total", "Total", paste(as.character(as.vector(unique_combinations[i_comb,])), collapse = "_"))
+    print(segmentName)
+    
+    
+    
+    # Prepare for balancing loop    
+    score_mean <- c(1) 
+    a_score_mean <- c(1)
+    b_score_mean <- c(1)
+    c_score_mean <- c(1)
+    score_pValue <- c(1) 
+    a_score_pValue <- c(1) 
+    b_score_pValue <- c(1) 
+    c_score_pValue <- c(1) 
+    score<-rbind(score_mean,score_pValue)
+    p=0
+    
+    while (sum(score > mean_difference) > 0)
+    {  
+      
+      
+      #Draw random sample from population for Treatment
+      Treatment <- segment_input[sample(1:nrow(segment_input), buckets*nrow(segment_input)),]
+      #delete randomly drawn data from population
+      #segment_input_1 <- segment_input[!(segment_input$ID %in% Treatment$ID),]
+      #Draw random sample from population for Control
+      Control <- segment_input[!(segment_input$ID %in% Treatment$ID),]
+      # Summarize results on entity level & add assigment and segment to data sets      
+      Treatment<-summarise_all(group_by_(Treatment[c(entity,names(Treatment)[sapply(Treatment, is.numeric)])],.dots = entity),funs(sum))
+      Control<-summarise_all(group_by_(Control[c(entity,names(Control)[sapply(Control, is.numeric)])],.dots = entity), funs(sum))
+      Treatment$Segment<- 'Treatment'
+      Control$Segment<-'Control'
+      Control$SegmentName <- segmentName
+      Treatment$SegmentName <- segmentName
+      # Update matrix to check the while condition      
+      score_mean <- c() 
+      score_pValue <- c() 
+      # calculate for minimum mean difference and Wilcox test   
+      for (i in 1: length(balancing)) {
+        
+        #  score_pValue[[balancing[i]]]<-wilcox.test(Treatment[[balancing[i]]],Control[[balancing[i]]])$p.value         
+        score_mean[[balancing[i]]]<-abs(mean(Treatment[[balancing[i]]]) - mean(Control[[balancing[i]]]))/mean(Control[[balancing[i]]])
+        
+    
+        test <- t.test(Treatment[[balancing[i]]], Control[[balancing[i]]],
+                       var.equal = FALSE, alternative = "two.sided"
+                       #test <- t.test(subset(Treatment[[kpis[i]]], Treatment[[kpis[i]]] >0) , subset(Control[[kpis[i]]], Control[[kpis[i]]] >0) ,
+                       #              var.equal = FALSE, alternative = "two.sided"                
+                       
+        )
+        
+        
+        
+        score_pValue[[balancing[i]]]<- ifelse(test$conf.int[1]*test$conf.int[2] >0, 1, 0)
+        
+      }
+      score<-rbind(score_mean,score_pValue) 
+      p = p+1
+      if (sum(score > mean_difference) == 0| p ==1000) break
+    }
+    
+    
+    # Write all segments in one data set 
+    All <-rbind(All,Treatment,Control)
+
+  }
+}
+
+endtime = Sys.time() - starttime
+endtime
+
+
+
+write.table(All,outputname, sep=',', dec='.',row.names=F)
+
+
+
+
+
